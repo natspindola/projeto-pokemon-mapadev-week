5558728301280bad90b7c709c6234ecdb286d541
Natália Spindola <natspindola@hotmail.com>
2022-08-01 18:43:37 -0300

    seta voltar JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,28 @@
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
 btnAvancar.addEventListener("click", function () {
-    const cartoes = document.querySelectorAll(".cartao");
-    
-    cartaoAtual++;
-    console.log(cartaoAtual)
-    cartoes[cartaoAtual].classList.add("selecionado");
+
+    if(cartaoAtual === cartoes.length - 1) return
 
     const cartaoSelecionado = document.querySelector(".selecionado");
     cartaoSelecionado.classList.remove("selecionado");
-    
+
+    cartaoAtual++;
+    console.log(cartaoAtual)
+    cartoes[cartaoAtual].classList.add("selecionado");
+});
+
+btnVoltar.addEventListener("click", function () {
+
+    if(cartaoAtual === 0) return
+
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+    cartaoAtual--;
+    console.log(cartaoAtual)
+    cartoes[cartaoAtual].classList.add("selecionado");
 });
