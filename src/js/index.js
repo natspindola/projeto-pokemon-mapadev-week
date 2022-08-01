462cbfc82ae7cbdbac0b367ed86575e4ba46f2c6
Natália Spindola <natspindola@hotmail.com>
2022-08-01 18:28:09 -0300

    correções

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
-const btnAvancar = document.getElementById('btn-avancar');
-const cartaoAtual = 0;
+const btnAvancar = document.getElementById("btn-avancar");
+let cartaoAtual = 0;
 
-btnAvancar.addEventListener('click', function (){
-    const cartoes = document.querySelectorAll('.cartao');
+btnAvancar.addEventListener("click", function () {
+    const cartoes = document.querySelectorAll(".cartao");
+    
     cartaoAtual++;
-    cartoes[cartaoAtual].classList.add('selecionado');
+    console.log(cartaoAtual)
+    cartoes[cartaoAtual].classList.add("selecionado");
+
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+    
 });
