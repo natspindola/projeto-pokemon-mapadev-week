8da3379df63cb412ab37cbea36bbfc12fb1be336
Natália Spindola <natspindola@hotmail.com>
2022-08-01 19:00:48 -0300

    finalização projeto

--- src/js/index.js
+++ src/js/index.js
@@ -3,26 +3,29 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
+}
+
 btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
 
-    if(cartaoAtual === cartoes.length - 1) return
+  esconderCartaoSelecionado();
 
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
-
-    cartaoAtual++;
-    console.log(cartaoAtual)
-    cartoes[cartaoAtual].classList.add("selecionado");
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
 });
 
 btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
 
-    if(cartaoAtual === 0) return
+  esconderCartaoSelecionado();
 
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
-
-    cartaoAtual--;
-    console.log(cartaoAtual)
-    cartoes[cartaoAtual].classList.add("selecionado");
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
 });
